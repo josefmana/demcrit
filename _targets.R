bb01e45671c3d407849724eea6d0d4b6cdba2292
Josef Mana <josef.mana@protonmail.com>
2025-06-25 13:20:53 +0200

    Put manuscript rendering directly inside the _targets script.

--- _targets.R
+++ _targets.R
@@ -21,36 +21,40 @@ list(
   tar_files(
     name = inputs, # Monitor input data files
     command = data_paths("data-raw"),
-    format  = "file"
+    format = "file"
   ),
   tar_target(
-    name = raw_data,
-    command = prepare_data(inputs)
-  ),
-  tar_target(
-    name    = pdd_data,
-    command = diagnose_pdd_sample(raw_data)
-  ),
-  tar_target(
-    name = variables,
+    name = variables, # List variables for future use
     command = here::here("data-raw","VariablesOfInterest.csv"),
     format = "file"
   ),
   tar_target(
-    name = sample_description,
+    name = raw_data, # Pre-process input data
+    command = prepare_data(inputs)
+  ),
+  tar_target(
+    name = pdd_data, # Assign probable PDD
+    command = diagnose_pdd_sample(raw_data)
+  ),
+  tar_target(
+    name = sample_description, # Describe the sample
     command = compute_descriptives(raw_data, variables)
   ),
   tar_target(
-    name = prevalence_summaries,
+    name = rate_summaries, # Summarise PDD rates
     command = summarise_rates(pdd_data, variables)
   ),
   tar_target(
-    name = demographic_predictors,
+    name = demographic_predictors, # Regress probable PDD on demographics
     command = regress_pdd_on_demographics(raw_data, pdd_data$PDD)
   ),
   tar_target(
-    name = concordance_statistics,
+    name = concordance_statistics, # Describe concordance between different PDD algorithms
     command = describe_concordance(pdd_data)
   ),
-  NULL
+  tar_quarto(
+    name = manuscript, # Prepare the manuscript
+    path = here::here("_manuscript", "manuscript.qmd"),
+    quiet = FALSE
+  )
 )
